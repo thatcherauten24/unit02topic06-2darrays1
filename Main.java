a7ab18c5915abee998400d887054d95947c244b5
Thatcher Auten <thatcherauten24@gmail.com>
2023-02-21 11:45:13 -0600

    copyRectangle completed

--- Main.java
+++ Main.java
@@ -14,15 +14,42 @@ public class Main {
     return output;
   }
 
+  public static int[][] copyRectangle(int[][] input) {
+    int rows = 4;
+    int cols = 3;
+
+    int[][] output = new int[rows][cols];
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < cols; c++) {
+        output[r][c] = input[r][c];
+      }
+    }
+    return output;
+
+  }
+
+  public static int[][] copyRagged(int[][] input) {
+
+    for (int[] row : input) {
+      for (int num : row) {
+        input[num]; 
+      }
+    }
+
+    
+  }
   
   public static void main(String[] args) {
-    int[][] orig = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int[][] newArr = copySquare(orig);
+    //int[][] origSquareArr = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    //int[][] newSquareArr = copySquare(squareArr);
+    //System.out.println(Arrays.deepToString(newSquareArr));
 
-    System.out.println("do they live at the same memory address...? " + orig + ", and " + newArr);
-    System.out.println("do they have the same contents?");
-    System.out.println(Arrays.deepToString(orig));
-    System.out.println(Arrays.deepToString(newArr));
+    //int[][] origRectArr = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+    //int[][] newRectArr = copyRectangle(origRectArr);
+    //System.out.println(Arrays.deepToString(newRectArr));
 
+    int[][] origRagArr = new int[][] {{1, 2, 3}, {4, 5, 6, 7}, {8, 9}};
+    int[][] newRagArr = copyRagged(origRagArr);
+    System.out.println(Arrays.deepToString(newRagArr));
   }
 }
